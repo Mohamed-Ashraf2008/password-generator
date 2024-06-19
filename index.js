e129ec64393179fa44302b37e269cbe4476bfebb
Mohamed-Ashraf2008 <151741823+Mohamed-Ashraf2008@users.noreply.github.com>
2024-06-20 02:21:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,20 +5,21 @@ const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","
 let btnEL = document.getElementById("btn-el")
 let password1EL = document.getElementById("password1-el")
 let password2EL = document.getElementById("password2-el")
+let passwordLength = parseInt(prompt("Enter the password length:"))
 
 function random (){
     let i = (Math.floor(Math.random()*characters.length))
     return (characters[i])
 }
 function looper (){
-    for (let j = 0 ; j < 16 ; j++){
+    for (let j = 0 ; j < passwordLength ; j++){
         
         return(random())
     }
 }
 function collector() {
     let result = ''
-    for (let l = 0; l < 15; l++) {
+    for (let l = 0; l < passwordLength; l++) {
         result += looper()
     }
     return result
