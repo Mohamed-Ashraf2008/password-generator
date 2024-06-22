ed44691f34cb4e0eeab756833a6180ab3d173702
Mohamed-Ashraf2008 <151741823+Mohamed-Ashraf2008@users.noreply.github.com>
2024-06-22 19:37:12 +0300

    BIG BIG CHANGES!
    i made it so you can copy the passwords and added some hover and active transtions

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","
 let btnEL = document.getElementById("btn-el")
 let password1EL = document.getElementById("password1-el")
 let password2EL = document.getElementById("password2-el")
-let passwordLength = parseInt(prompt("Enter the password length:"))
+let passwordLength = parseInt(prompt("Enter the password length:")) 
 
 function random (){
     let i = (Math.floor(Math.random()*characters.length))
@@ -28,4 +28,23 @@ function collector() {
 function generate(){
     password1EL.textContent = collector()
     password2EL.textContent = collector()
+}
+function copy1(){
+    if (password1EL.textContent === "" || password1EL.textContent === "copied") {
+        return; // Do nothing if the password is empty or already copied
+    }
+    navigator.clipboard.writeText(password1EL.textContent)
+    let copy = "copied"
+    password1EL.textContent = copy
+    window.alert("Password copied to clipboard");
+}
+
+function copy2() {
+    if (password2EL.textContent === "" || password2EL.textContent === "copied") {
+        return; // Do nothing if the password is empty or already copied
+    }
+    navigator.clipboard.writeText(password2EL.textContent)
+    let copy = "copied"
+    password2EL.textContent = copy
+    window.alert("Password copied to clipboard");
 }
