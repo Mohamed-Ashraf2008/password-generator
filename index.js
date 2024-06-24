e6ab8910dd8d925278002c513e5ddab807bdc2ec
Mohamed-Ashraf2008 <151741823+Mohamed-Ashraf2008@users.noreply.github.com>
2024-06-24 07:52:26 +0300

    its too much things my brain is not braining rn sry future me

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
-const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+let letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+let symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"]
 
+
+let characters = letters.concat(numbers , symbols)
+console.log(characters)
 
 let btnEL = document.getElementById("btn-el")
 let password1EL = document.getElementById("password1-el")
 let password2EL = document.getElementById("password2-el")
-let passwordLength = parseInt(prompt("Enter the password length:")) 
-
+let input = document.getElementById("length-el")
+let passwordLength = input.value
+let orginalLength = passwordLength
+let popup = document.getElementById("popup-el")
+let includeNumbersEl = document.getElementById("include-numbers-el");
+let includeNumbers 
+let includeSymbolsEl = document.getElementById("include-symbols-el");
+let includeSymbols  = includeSymbolsEl.checked
+function sumit(){
+    passwordLength = input.value
+    includeNumbers = includeNumbersEl.checked;
+    console.log(includeNumbers)
+    popup.classList.remove("open-popup")
+    if (includeNumbers === false) {
+        characters = letters.concat(symbols);
+    } else if (includeNumbers === true) {
+        characters = letters.concat(numbers, symbols);
+    }
+    
+}
+function reset(){
+    passwordLength = orginalLength
+    input.value = orginalLength
+    popup.classList.remove("open-popup")
+}
 function random (){
     let i = (Math.floor(Math.random()*characters.length))
     return (characters[i])
@@ -36,7 +64,6 @@ function copy1(){
     navigator.clipboard.writeText(password1EL.textContent)
     let copy = "copied"
     password1EL.textContent = copy
-    window.alert("Password copied to clipboard");
 }
 
 function copy2() {
@@ -46,5 +73,7 @@ function copy2() {
     navigator.clipboard.writeText(password2EL.textContent)
     let copy = "copied"
     password2EL.textContent = copy
-    window.alert("Password copied to clipboard");
+}
+function settings(){
+    popup.classList.add("open-popup")
 }
